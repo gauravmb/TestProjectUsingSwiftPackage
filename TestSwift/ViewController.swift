a70f33d3a75057dcb370222c284f9fcace565f71
gaurav.bhatia <gaurav.bhatia@hurix.com>
2020-06-17 16:42:11 +0530

    First Commit

---
+++ TestSwift/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestSwift
+//
+//  Created by Gaurav Bhatia on 17/06/20.
+//  Copyright © 2020 Gaurav Bhatia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+    
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
